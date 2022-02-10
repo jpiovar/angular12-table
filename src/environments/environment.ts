d9e1dea88dceca924abc9f756e24fb670c534e15
Juraj Piovar <jpiovar@sk.ibm.com>
2022-02-10 16:43:46 +0100

    AZURE env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,8 +13,8 @@ export const environment = {
   beTableDataBaseEndPoint: 'table-data-base',
   redirectUrl: 'http://localhost:4200',
   cloudUrl: 'https://login.microsoftonline.com',
-  clientId: process.env.CLIENT_ID,
-  tenantId: process.env.TENANT_ID
+  clientId: process.env.AZURE_CLIENT_ID,
+  tenantId: process.env.AZURE_TENANT_ID
 };
 
 /*
