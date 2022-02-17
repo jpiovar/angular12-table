50461eae9945abf87a5c7c18b3d2986cfcb44716
Juraj Piovar <jpiovar@sk.ibm.com>
2022-02-17 15:27:33 +0100

    .env variables AZURE_BE_ORIGIN , AZURE_REDIRECT_URL , AZURE_CLOUD_URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  beOrigin: 'http://localhost:3000/',
+  beOrigin: process.env.AZURE_BE_ORIGIN,
   beHomeEndPoint: 'home',
   beVersionEndPoint: 'version',
   beTableDataEndPoint: 'table-data',
   beMetaDataEndPoint: 'meta-data',
   beTableChangeLogs: 'table-change-logs',
   beTableDataBaseEndPoint: 'table-data-base',
-  redirectUrl: 'http://localhost:4200',
-  cloudUrl: 'https://login.microsoftonline.com',
+  redirectUrl: process.env.AZURE_REDIRECT_URL,
+  cloudUrl: process.env.AZURE_CLOUD_URL,
   clientId: process.env.AZURE_CLIENT_ID,
   tenantId: process.env.AZURE_TENANT_ID
 };
