ce2465b8294a3a7ad9285e996507b46fbc1afc32
Juraj Piovar <jpiovar@sk.ibm.com>
2022-03-14 14:12:33 +0100

    startSpinner before resolving request load

--- src/app/records.resolver.ts
+++ src/app/records.resolver.ts
@@ -4,24 +4,30 @@ import { select, Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 import { delay, filter, first, map, tap } from 'rxjs/operators';
 import { AppState } from './state';
+import { StartSpinner, StopSpinner } from './state/spinner/spinner.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecordsResolver implements Resolve<boolean|any> {
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>) {
+    // this.store.dispatch(new StartSpinner());
+  }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean|any> {
     // return of(true).pipe(delay(2000));
+    this.store.dispatch(new StartSpinner());
     return this.store.pipe(
       select('user'),
       tap(state => {
         // debugger;
-        console.log('state', state);
+        console.log('resolver tap user state ', state);
       }),
       filter(state => state && state.accessToken !== null),
       map(state => {
         // debugger;
+        console.log('resolver map user state ', state);
+        // this.store.dispatch(new StopSpinner());
         return state;
       }),
       first()
