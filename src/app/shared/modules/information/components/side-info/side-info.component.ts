b4637bfe985bac2add5145820afdf6496f5c53ef
Juraj Piovar <jpiovar@sk.ibm.com>
2022-04-20 14:46:13 +0200

    popover open after init

--- src/app/shared/modules/information/components/side-info/side-info.component.ts
+++ src/app/shared/modules/information/components/side-info/side-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-side-info',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideInfoComponent implements OnInit {
 
+  @ViewChild('popover1') public popover: NgbPopover;
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      if (!this.popover.isOpen()) {
+        this.popover.open();
+      }
+    });
   }
 
 }
